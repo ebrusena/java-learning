e238f5b8c45184a3279e01e99fb7c37a6a9f051c
Ebru Sena Ünsal <ebrusenaunsal@posta.mu.edu.tr>
2026-08-13 15:29:43 +0300

    methodlar

---
+++ src/method/methods.java
@@ -0,0 +1,21 @@
+package method;
+
+public class methods {
+    public static double kdvhesapla(double  urunFiyati){
+        double kdvMiktari = urunFiyati * 0.20;
+        double toplamFiyat = urunFiyati + kdvMiktari;
+
+        return toplamFiyat;
+
+
+
+    }
+
+    static void main(String[] args) {
+        double ayakkabiSonFiyati = kdvhesapla(1000);
+        System.out.println("ayakkabının kdvli fiyatı"+ayakkabiSonFiyati+"Tl'dir.");
+
+        double telefonSonFiyat = kdvhesapla(1111567);
+        System.out.println("telefonun kdvli fiyatı"+telefonSonFiyat+"Tl");
+    }
+}
